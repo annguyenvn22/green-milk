cac8234b93bec121c804b9be2e2ace5aca2072be
AnNVC <annvcit@gmail.com>
2016-08-27 11:43:33 +0700

    commit to branch develop Next step: modify the whole web page layout using right bootstrap's col-*-* classes

--- modules/checkout/client/contronllers/admin/checkout-detail.client.controller.js
+++ modules/checkout/client/contronllers/admin/checkout-detail.client.controller.js
@@ -1,20 +1,20 @@
 (function() {
     'use strict';
-    
+
     angular
         .module('checkout.admin')
         .controller('CheckoutDetailController', CheckoutDetailController);
-        
+
     CheckoutDetailController.$inject = ['$stateParams', 'uiGridConstants'];
-    
+
     function CheckoutDetailController($stateParams, uiGridConstants) {
         var vm = this;
-        
+
         activate();
-        
+
         /////////////
-        
-        
+
+
         function activate() {
             vm.customerNameLabel = 'Tên khách hàng: ';
             vm.customerAddressLabel = 'Địa chỉ: ';
@@ -24,27 +24,39 @@
             vm.saveLabel = 'Lưu';
             vm.cancelLabel = 'Hủy';
             vm.releaseDateLabel = 'Ngày giao';
-            vm.gridOptions= {
+            vm.options = [
+                {display: 'Đã giao', value: 'resovled'},
+                {display: 'Chưa giao', value: 'pending'}
+            ];
+            vm.gridOptions = {
                 showColumnFooter: true,
-                data:[],
-                columnDefs: [
-                        {
-                    field: 'index', displayName: '#', width: '10%', cellTemplate: '<div class="ui-grid-cell-contents">{{grid.renderContainers.body.visibleRowCache.indexOf(row) + 1}}</div>'
-                            },
-                            {
-                    field: 'name', displayName: 'Tên Sữa'
-                            },
-                            {
-                    field: 'amount', displayName: 'Số Lượng'
-                            },
-                            {
-                    field: 'price', displayName: 'Giá', type: 'number', cellTemplate: '<div>{{ grid.appScope.$parent.vm.CartMonthlyService.totalWeekMoney() | currency : "" : 0 }} đ</div>'
-                            },
-                            {
-                    name: 'totalPrice', displayName: 'Thành tiền', type: 'number', aggregationType: uiGridConstants.aggregationTypes.sum, cellTemplate: '<div>{{ row.entity.price * row.entity.amount  | currency : "" : 0 }} đ</div>', footerCellTemplate: '<div>Tổng Tiền: {{ grid.appScope.$parent.vm.service.totalMoney() | currency : "" : 0 }} đ</div>'
-                            }
-                            
-                        ]
+                data: [],
+                columnDefs: [{
+                        field: 'index',
+                        displayName: '#',
+                        width: '10%',
+                        cellTemplate: '<div class="ui-grid-cell-contents">{{grid.renderContainers.body.visibleRowCache.indexOf(row) + 1}}</div>'
+                    }, {
+                        field: 'name',
+                        displayName: 'Tên Sữa'
+                    }, {
+                        field: 'amount',
+                        displayName: 'Số Lượng'
+                    }, {
+                        field: 'price',
+                        displayName: 'Giá',
+                        type: 'number',
+                        cellTemplate: '<div>{{ grid.appScope.$parent.vm.CartMonthlyService.totalWeekMoney() | currency : "" : 0 }} đ</div>'
+                    }, {
+                        name: 'totalPrice',
+                        displayName: 'Thành tiền',
+                        type: 'number',
+                        aggregationType: uiGridConstants.aggregationTypes.sum,
+                        cellTemplate: '<div>{{ row.entity.price * row.entity.amount  | currency : "" : 0 }} đ</div>',
+                        footerCellTemplate: '<div>Tổng Tiền: {{ grid.appScope.$parent.vm.service.totalMoney() | currency : "" : 0 }} đ</div>'
+                    }
+
+                ]
             };
         }
     }
