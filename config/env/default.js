28ea83e9856c3789e5595c12db51908d0ac8b9dd
AnNVC <annvcit@gmail.com>
2016-09-05 05:32:14 +0700

    create 3 app facebook 1 for production 1 for localhost 1 for c9

--- config/env/default.js
+++ config/env/default.js
@@ -1,31 +1,31 @@
 'use strict';
 
 module.exports = {
-    app              : {
-        title      : 'Green Milk',
+    app: {
+        title: 'Green Milk',
         description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-        keywords   : 'mongodb, express, angularjs, node.js, mongoose, passport, green-milk, green milk, Green-Milk, Green Milk, GreenMilk'
+        keywords: 'mongodb, express, angularjs, node.js, mongoose, passport, green-milk, green milk, Green-Milk, Green Milk, GreenMilk'
     },
-    port             : process.env.PORT || 3000,
-    host             : process.env.HOST || 'localhost',
-    templateEngine   : 'pug',
+    port: process.env.PORT || 3000,
+    host: process.env.HOST || 'localhost',
+    templateEngine: 'pug',
     // Session Cookie settings
-    sessionCookie    : {
+    sessionCookie: {
         // session expiration is set by default to 24 hours
-        maxAge  : 24 * (60 * 60 * 1000),
+        maxAge: 24 * (60 * 60 * 1000),
         // httpOnly flag makes sure the cookie is only accessed
         // through the HTTP protocol and not JS/browser
         httpOnly: true,
         // secure cookie should be turned to true to provide additional
         // layer of security so that the cookie is set only when working
         // in HTTPS mode.
-        secure  : false
+        secure: false
     },
     // sessionSecret should be changed for security measures and concerns
-    sessionSecret    : process.env.SESSION_SECRET || 'G33n-M1Lk',
+    sessionSecret: process.env.SESSION_SECRET || 'G33n-M1Lk',
     // sessionKey is the cookie session name
-    sessionKey       : 'sessionId',
+    sessionKey: 'sessionId',
     sessionCollection: 'sessions',
-    favicon          : 'modules/core/client/img/brand/favicon (2).ico'
+    favicon: 'modules/core/client/img/brand/favicon (2).ico'
 };
 
